ae6d36f47e9926321d5dfe7597c2d59e5e9b4e39
Crios <87044034+darthcrios@users.noreply.github.com>
2025-02-12 22:59:36 -0800

    heading updates

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
             data.forEach(post => {
                 const article = document.createElement('article');
                 article.innerHTML = `
-                  <h2><a href="${post.url}" style="color: white;">${post.title}</a></h2>
+                  <h3><a href="${post.url}" style="color: white;">${post.title}</a></h3>
                     <p style="color: white;">Published on: <time datetime="${post.date}">${new Date(post.date).toDateString()}</time></p>
                     <p style="color: white;">${post.summary}</p>
                     <a href="${post.url}" style="color: green;">Read more...</a>
