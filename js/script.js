43b63ee7892c8ae9b926619d763478dca24cf6d1
Crios <87044034+darthcrios@users.noreply.github.com>
2025-11-23 21:28:30 -0800

    WAV swap to MP3

--- js/script.js
+++ js/script.js
@@ -20,47 +20,47 @@ const tracksData = [
   {
     title: "Vibe With Me",
     albumArt: "AlbumArt/VWH4.jpg",
-    audio: "Masters/Críos - Vibe With Me.wav"
+    audio: "Masters/Críos - Vibe With Me.mp3"
   },
   {
     title: "Kollective - Riverbend Remix",
     albumArt: "AlbumArt/SinglesRemix.jpg",
-    audio: "Masters/Críos - Riverbend (Mastered with Sunroof at 55pct).wav"
+    audio: "Masters/Críos - Riverbend (Mastered with Sunroof at 55pct).mp3"
   },
   {
     title: "Keep Moving",
     albumArt: "AlbumArt/Keep Moving.jpg",
-    audio: "Masters/Críos - Keep Moving.wav"
+    audio: "Masters/Críos - Keep Moving.mp3"
   },
   {
     title: "You Got the Feeling Ft Alan Martin",
     albumArt: "AlbumArt/SinglesCover.jpg",
-    audio: "Masters/Críos - You Got The Feeling.wav"
+    audio: "Masters/Críos - You Got The Feeling.mp3"
   },
   {
     title: "Killer Vibes (Free Download)",
     albumArt: "AlbumArt/KillerVibesArt.jpg",
-    audio: "Masters/Críos - Killer Vibes(Demo Exclusive).wav"
+    audio: "Masters/Críos - Killer Vibes(Demo Exclusive).mp3"
   },
   {
     title: "LA Rain",
     albumArt: "AlbumArt/LA Rain.jpg",
-    audio: "Masters/Críos - LARain.wav"
+    audio: "Masters/Críos - LARain.mp3"
   },
   {
     title: "Isolated Red",
     albumArt: "AlbumArt/red_isolated.jpg",
-    audio: "Masters/Críos - Isolated Red.wav"
+    audio: "Masters/Críos - Isolated Red.mp3"
   },
   {
     title: "Puca",
     albumArt: "AlbumArt/Beach Ghost.jpeg",
-    audio: "Masters/Críos - Ghost on the beach(Ghostwave Contest).wav"
+    audio: "Masters/Críos - Ghost on the beach(Ghostwave Contest).mp3"
   },
   {
     title: "Move Yo Body Ft Alex Arcana",
     albumArt: "AlbumArt/movyobody2.jpg",
-    audio: "Masters/Críos - MoveyoBody.wav"
+    audio: "Masters/Críos - MoveyoBody.mp3"
   },
 
 ];
@@ -78,7 +78,7 @@ tracksData.forEach(track => {
     <img src="${track.albumArt}" alt="${track.title} Album Art">
     <div class="track-title">${track.title}</div>
     <div class="track-wave"></div>
-    <audio controls>
+    <audio controls preload="none">
       <source src="${track.audio}" type="${type}">
       Your browser does not support the audio element.
     </audio>
